dca1af70b065432a18eb065e19c9e26a7050a72c
pax <veganbikepunk@gmail.com>
2019-10-10 17:28:08 -0700

    made toggle buttons correspond to user preferences

--- app/src/main/java/me/paxana/ingredientreader/IngredientReader.kt
+++ app/src/main/java/me/paxana/ingredientreader/IngredientReader.kt
@@ -1,12 +1,22 @@
 package me.paxana.ingredientreader
 
 import android.app.Application
+import android.content.ContextWrapper
 import com.orhanobut.logger.AndroidLogAdapter
 import com.orhanobut.logger.Logger
+import com.pixplicity.easyprefs.library.Prefs
 
 class IngredientReader: Application() {
     override fun onCreate() {
         super.onCreate()
         Logger.addLogAdapter(AndroidLogAdapter())
+
+        // Initialize the Prefs class
+        Prefs.Builder()
+            .setContext(this)
+            .setMode(ContextWrapper.MODE_PRIVATE)
+            .setPrefsName(packageName)
+            .setUseDefaultSharedPreference(true)
+            .build()
     }
 }
--- app/src/main/java/me/paxana/ingredientreader/ProfileActivity.kt
+++ app/src/main/java/me/paxana/ingredientreader/ProfileActivity.kt
@@ -3,7 +3,7 @@ package me.paxana.ingredientreader
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.amazonaws.mobile.client.AWSMobileClient
-import com.orhanobut.logger.Logger
+import com.pixplicity.easyprefs.library.Prefs
 import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
@@ -13,5 +13,20 @@ class ProfileActivity : AppCompatActivity() {
         setContentView(R.layout.activity_profile)
 
         textView.text = "Hello ${AWSMobileClient.getInstance().username}!"
+
+        debug_toggle.isChecked = Prefs.getBoolean("DEBUG_KEY", false)
+        sugar_toggle.isChecked = Prefs.getBoolean("SUGAR_KEY", true)
+        palm_toggle.isChecked = Prefs.getBoolean("PALM_KEY", true)
+
+        debug_toggle.setOnCheckedChangeListener { buttonView, isChecked ->
+            Prefs.putBoolean("DEBUG_KEY", isChecked)
+        }
+        sugar_toggle.setOnCheckedChangeListener { buttonView, isChecked ->
+            Prefs.putBoolean("SUGAR_KEY", isChecked)
+        }
+        palm_toggle.setOnCheckedChangeListener { buttonView, isChecked ->
+            Prefs.putBoolean("PALM_KEY", isChecked)
+        }
+
     }
 }
